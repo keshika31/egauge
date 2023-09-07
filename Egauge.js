6d7c853f9ac7eb3d9561e70b76df22747b9cb959
keshika31 <144307783+keshika31@users.noreply.github.com>
2023-09-07 23:29:40 +0400

    Update Egauge.js

--- Egauge.js
+++ Egauge.js
@@ -14,7 +14,102 @@ var getScriptPromisify = (src) => {
 
     class SampleTemplate extends HTMLElement {
         constructor() {
-            super
+            super()
+            this._shadowRoot = this.attachShadow({
+                mode: 'open'
+            })
+            this._shadowRoot.appendChild(template.content.cloneNode(true))
+            this._root = this._shadowRoot.getElementByID('root')
+            this._props = {}
+            this.render()
+        }
+
+        onCustomWidgetResize(width, height) {
+            this.render()
+        }
+
+        async render(value) {
+            await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts/min/js')
+
+            const chart = echarts.init(this._root, 'dark')
+            const option = {
+                series: [{
+                    type: 'gauge',
+                    center: ["50%", "60%"],
+                    startAngle: 200,
+                    endAngle: -20,
+                    min: 0,
+                    max: 20,
+                    splitNumber: 1,
+                    itemStyle: {
+                        color: '#FFAB91'
+                    },
+
+                    progress: {
+                        show: true,
+                        width: 30
+                    },
+
+                    pointer: {
+                        show: false
+                    },
+
+                    axisLine: {
+                        lineStyle: {
+                            width: 30
+                        }
+                    },
+
+                    axisTrick: {
+                        distance: -45,
+                        splitNumber: 5,
+                        lineStyle: {
+                            width: 2,
+                            color: '#999'
+                        }
+                    },
+
+                    splitLine: {
+                        distance: -52,
+                        length: 14,
+                        lineStyle: {
+                            width: 3,
+                            color: '#999'
+                        }
+                    },
+
+                    axisLabel: {
+                        distance: -20,
+                        color: '#999',
+                        fontSize: 20
+                    },
+
+                    anchoe: {
+                        show: false
+                    },
+
+                    title: {
+                        show: false
+                    },
+
+                    detail: {
+                        valueAnimation: true,
+                        width: '60%',
+                        lineHeight: 40,
+                        height: '15%'
+                        borderRadius: 8,
+                        offsetCenter: [0, '-15%'],
+                        fontSize: 60,
+                        fontWeight: 'bolder',
+                        formatter: '$ {value}',
+                        color: 'auto'
+                    },
+
+                    data: {
+                        value: value?value:0
+                    }
+                }]
+            }
         }
     }
 })
